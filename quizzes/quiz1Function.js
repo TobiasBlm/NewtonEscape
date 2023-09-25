46537c9f52b7dbf85989bba5a234df5803bb0c47
Tobias Blume <tobigblume@gmail.com>
2023-09-25 22:31:53 +0200

    finishing touches, only Jannik missing

--- quizzes/quiz1Function.js
+++ quizzes/quiz1Function.js
@@ -8,7 +8,7 @@ function berechne() {
     const berechnetesErgebnis = (masse * g * hoehenunterschied) / zeit;
 
     if (Math.round(berechnetesErgebnis * 10) / 10 === parseFloat(ergebnis)) {
-        alert("Gut gemacht! Die Lösungsbuchstaben sind f,a,t,g");
+        alert("Gut gemacht! Der Lösungsbuchstabe ist: B");
     } else {
         alert("Das Ergebnis ist falsch! Versuche es nochmal!");
     }
